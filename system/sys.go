b68bc2618b9bef3c23ef2cb5043ddf46244c8a19
Yves Junqueira <yves.junqueira@gmail.com>
2012-04-16 06:34:23 +0200

    Code reorganization

--- system/sys.go
+++ system/sys.go
@@ -1,26 +1,32 @@
+// CHIP-8 emulator.
+//
+// nictuku@gamerom.org
+//
+// References:
+// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+// http://en.wikipedia.org/wiki/CHIP-8
+// http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
 package system
 
 import (
-	"errors"
 	"log"
 	"time"
-
-	"github.com/0xe2-0x9a-0x9b/Go-SDL/sdl"
 )
 
 func New() *Sys {
-	mem := make(memory, 4096)
+	mem := make(memory, ramCapacity)
 
-	// Build fontset.
+	// XXX Should this be after 0x50? Find a program that uses it, then try.
 	copy(mem, fontset)
 
 	return &Sys{
-		v:   make([]byte, 16),
-		pc:  0x200,
-		i:   0x200,
-		sp:  0,
-		mem: mem,
-		gfx: make([]byte, screenWidth*screenHeight),
+		v:     make([]byte, numRegisters),
+		pc:    programAreaStart,
+		i:     programAreaStart,
+		sp:    0,
+		mem:   mem,
+		gfx:   make([]byte, screenWidth*screenHeight),
+		video: new(video),
 	}
 }
 
@@ -33,78 +39,64 @@ type Sys struct {
 	gfx        []byte
 	delayTimer byte
 	soundTimer byte
+	video      *video
 }
 
 func (s *Sys) Init() error {
-	if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
-		return errors.New(sdl.GetError())
-	}
-
-	screen := sdl.SetVideoMode(640, 320, 8, 0)
-	if screen == nil {
-		return errors.New(sdl.GetError())
-	}
-
-	sdl.EnableUNICODE(1)
-
-	sdl.WM_SetCaption("nictuku's CHIP-8 emulator", "") // no icon.
-
-	screen.FillRect(nil, 0x302019)
-
-	return nil
+	return s.video.init()
 }
 
 func (s *Sys) LoadGame(rom []byte) {
 	if len(rom) == 0 {
 		log.Fatal("Tried to load zero-length ROM.")
 	}
-	romArea := s.mem[0x200:]
+	romArea := s.mem[programAreaStart:]
 	copy(romArea, rom)
-	log.Printf("rom loaded %04x", s.mem[0x202])
+	log.Println("rom loaded")
 }
 
 func (s *Sys) Run() error {
-	for _ = range [1024]bool{} {
+	tick := time.Tick(time.Second / cpuFrequency) // 60hz.
+	for {
+		<-tick
 		s.stepCycle()
 	}
-	log.Println("run done")
 	return nil
 }
 
 func (s *Sys) stepCycle() {
 
 	draw := false
-
 	// pc points to next opcode.
-	opcode := uint16(s.mem[s.pc])<<8 | uint16(s.mem[s.pc+1])
 	log.Printf("opcode 0x%04x", opcode)
+	// TODO(nictuku): Implement a proper tracer. Make it generic.
 	log.Printf("pc 0x%04x", s.pc)
 
 	masked := opcode & 0xF000
-	log.Printf("%x", masked)
 	switch masked {
-
 	case 0x0000:
+		// 0NNN	Calls RCA 1802 program at address NNN.
+		//  => Only used by the original computers that implemented CHIP-8.
+
 		switch masked & 0x000F {
-		case 0x0000: // 00E0	Clears the screen.
-			// XXXX
-			log.Println("clearing screen")
-			sdl.GetVideoSurface().FillRect(nil, 0x302019)
+		case 0x0000:
+			// 00E0	Clears the screen.
+			s.gfx = make([]byte, screenWidth*screenHeight)
+			draw = true
 			s.pc += 2
-			// 00EE	Returns from a subroutine.
 		default:
+			// 00EE	Returns from a subroutine.
 			log.Printf("opcode not implemented: %x", opcode)
 			return
 		}
-		//	default:
-		//		// 0NNN	Calls RCA 1802 program at address NNN.
-		//		s.pc = opcode
 
-	// 1NNN	Jumps to address NNN.
 	case 0x1000:
+		// 1NNN	Jumps to address NNN.
 		s.pc = opcode & 0x0FFF
+
 	// 2NNN	Calls subroutine at NNN.
 	// 3XNN	Skips the next instruction if VX equals NN.
+
 	case 0x4000:
 		if s.v[(opcode&0x0F00)>>8] != byte(opcode&0x00FF) {
 			s.pc += 4
@@ -114,6 +106,7 @@ func (s *Sys) stepCycle() {
 
 	// 4XNN	Skips the next instruction if VX doesn't equal NN.
 	// 5XY0	Skips the next instruction if VX equals VY.
+
 	case 0x6000:
 		// 6XNN	Sets VX to NN.
 		vx := (opcode & 0x0F00) >> 8
@@ -123,7 +116,6 @@ func (s *Sys) stepCycle() {
 		// 7XNN	Adds NN to VX.
 		vx := (opcode & 0x0F00) >> 8
 		s.v[vx] += byte(opcode & 0x00FF)
-		// XXX increment VF.
 		s.pc += 2
 
 	// 8XY0	Sets VX to the value of VY.
@@ -137,11 +129,15 @@ func (s *Sys) stepCycle() {
 	// 8XYE	Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift.[2]
 	// 9XY0	Skips the next instruction if VX doesn't equal VY.
 
-	case 0xA000: // ANNN	Sets I to the address NNN.
-		s.i = opcode & 0x0FFF
-		s.pc += 2 // BNNN	Jumps to the address NNN plus V0.
+	case 0xA000:
+		// ANNN	Sets I to the address NNN.
 		log.Println("ANNN")
+		s.i = opcode & 0x0FFF
+		s.pc += 2
+
+	// BNNN	Jumps to the address NNN plus V0.
 	// CXNN	Sets VX to a random number and NN.
+
 	case 0xD000:
 		// DXYN	Draws a sprite at coordinate (VX, VY) that has a width of 8
 		// pixels and a height of N pixels. Each row of 8 pixels is read as
@@ -185,13 +181,14 @@ func (s *Sys) stepCycle() {
 	case 0xF000:
 		switch opcode & 0x00FF {
 		case 0x0015:
+			// FX15	Sets the delay timer to VX.
 			s.delayTimer = byte(opcode & 0x00FF)
 			s.pc += 2
 		default:
 			log.Printf("opcode not implemented: %x", opcode)
 			return
 		}
-		// FX15	Sets the delay timer to VX.
+
 		// FX18	Sets the sound timer to VX.
 		// FX1E	Adds VX to I.[3]
 		// FX29	Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
@@ -214,25 +211,6 @@ func (s *Sys) stepCycle() {
 	}
 
 	if draw {
-		surface := sdl.GetVideoSurface()
-		for yline := int16(0); yline < screenHeight; yline++ {
-			for xline := int16(0); xline < screenWidth; xline++ {
-				if s.gfx[xline+yline*64] == 0 {
-					// unsafe.Pointer, do arithm to find what to update.
-					surface.FillRect(&sdl.Rect{xline * 10, yline * 10, 10, 10},
-						sdl.MapRGBA(surface.Format, 255, 255, 255, 128),
-					)
-				} else {
-					surface.FillRect(&sdl.Rect{xline * 10, yline * 10, 10, 10},
-						sdl.MapRGBA(surface.Format, 0, 0, 0, 128),
-					)
-				}
-			}
-		}
-		surface.Flip()
-
+		s.video.draw(s.gfx)
 	}
-
-	// time.Sleep(time.Second / 5) // 60hz.
-	time.Sleep(time.Second / cpuFrequency) // 60hz.
 }
---
+++ system/video.go
@@ -0,0 +1,48 @@
+package system
+
+import (
+	"errors"
+
+	"github.com/0xe2-0x9a-0x9b/Go-SDL/sdl"
+)
+
+const (
+	// Multiplies screen size by 10.
+	pixelSize = 10
+)
+
+type video struct {
+}
+
+func (v *video) init() error {
+	if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
+		return errors.New(sdl.GetError())
+	}
+	screen := sdl.SetVideoMode(64*pixelSize, 32*pixelSize, 8, 0)
+	if screen == nil {
+		return errors.New(sdl.GetError())
+	}
+	sdl.EnableUNICODE(1)
+	sdl.WM_SetCaption("nictuku's CHIP-8 emulator", "") // no icon.
+	screen.FillRect(nil, 0x302019)
+	return nil
+}
+
+func (v *video) draw(pixels []byte) {
+	surface := sdl.GetVideoSurface()
+
+	// zero-out the screen.
+	// sdl.GetVideoSurface().FillRect(nil, 0x302019)
+
+	for yline := int16(0); yline < screenHeight; yline++ {
+		for xline := int16(0); xline < screenWidth; xline++ {
+			r := &sdl.Rect{xline * pixelSize, yline * pixelSize, pixelSize, pixelSize}
+			if pixels[xline+yline*64] == 0 {
+				surface.FillRect(r, sdl.MapRGBA(surface.Format, 255, 255, 255, 128))
+			} else {
+				surface.FillRect(r, sdl.MapRGBA(surface.Format, 0, 0, 0, 128))
+			}
+		}
+	}
+	surface.Flip()
+}
